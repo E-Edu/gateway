5f1159c195f1e3ea024d844be537a927763d4391
Steve Hb <stve.hb@gmail.com>
2020-03-25 18:26:13 +0100

    Rewrote base

---
+++ src/main/java/de/themorpheus/edu/gateway/graphql/dto/ServiceInfoDTO.java
@@ -0,0 +1,25 @@
+package de.themorpheus.edu.gateway.graphql.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceInfoDTO {
+
+	@NotNull @NotEmpty @NotBlank
+	private String name;
+	@NotNull @NotEmpty @NotBlank
+	private String version;
+	@NotNull @NotEmpty @NotBlank
+	private String commit;
+	@Min(0)
+	private int buildTime;
+
+}
