5c0c7f4c4a5d24d1d78251d105b52c1949b65954
Steve Hb <stve.hb@gmail.com>
2020-03-25 18:34:45 +0100

    Fixed checkstyle

--- src/main/java/de/themorpheus/edu/gateway/graphql/dto/ServiceInfoDTO.java
+++ src/main/java/de/themorpheus/edu/gateway/graphql/dto/ServiceInfoDTO.java
@@ -1,12 +1,13 @@
 package de.themorpheus.edu.gateway.graphql.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
--- src/main/java/de/themorpheus/edu/gateway/graphql/resolver/query/ServiceInfoResolver.java
+++ src/main/java/de/themorpheus/edu/gateway/graphql/resolver/query/ServiceInfoResolver.java
@@ -3,10 +3,10 @@ package de.themorpheus.edu.gateway.graphql.resolver.query;
 import de.themorpheus.edu.gateway.graphql.dto.ServiceInfoDTO;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.schema.DataFetchingEnvironment;
-import org.springframework.stereotype.Component;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceInfoResolver implements GraphQLQueryResolver {
