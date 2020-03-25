3b0b95edd40cfcaf155f9fd32288e8601c5ebf0e
Steve Hb <stve.hb@gmail.com>
2020-03-25 22:22:35 +0100

    Implemented real gateway info

--- src/main/java/de/themorpheus/edu/gateway/graphql/resolver/query/ServiceInfoResolver.java
+++ src/main/java/de/themorpheus/edu/gateway/graphql/resolver/query/ServiceInfoResolver.java
@@ -1,6 +1,7 @@
 package de.themorpheus.edu.gateway.graphql.resolver.query;
 
 import de.themorpheus.edu.gateway.graphql.dto.ServiceInfoDTO;
+import com.jcabi.manifests.Manifests;
 import org.springframework.stereotype.Component;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -11,13 +12,15 @@ import graphql.schema.DataFetchingEnvironment;
 @Component
 public class ServiceInfoResolver implements GraphQLQueryResolver {
 
+	private static final ServiceInfoDTO GATEWAY = new ServiceInfoDTO(
+		"Gateway",
+		ServiceInfoResolver.class.getPackage().getImplementationVersion(),
+		Manifests.read("Commit-Id"),
+		Long.parseLong(Manifests.read("Build-Time"))
+	);
+
 	public ServiceInfoDTO serviceInfoByName(@NotNull @NotEmpty @NotBlank String name, DataFetchingEnvironment environment) {
-		return new ServiceInfoDTO(
-			name,
-			"1.0",
-			"0a49608",
-			1234567
-		);
+		return GATEWAY;
 	}
 
 }
