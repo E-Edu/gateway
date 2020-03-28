7ef280d22f4632498246c0d15f46d38ac8ac1c22
Steve Hb <stve.hb@gmail.com>
2020-03-28 03:08:10 +0100

    Re-added CORS handling

--- src/main/java/de/themorpheus/edu/gateway/GatewayApplication.java
+++ src/main/java/de/themorpheus/edu/gateway/GatewayApplication.java
@@ -2,12 +2,30 @@ package de.themorpheus.edu.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer webConfiguration() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+					.addMapping("/graphql")
+					.allowedOrigins(
+						"*" //TODO: Not in production
+					)
+					.allowedMethods("GET", "OPTION", "POST", "PATCH", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
